4f3672fe5fdb331903dd08de39bd8db2b2ba1482
jonathandiaz <jonathan.diaz714@gmail.com>
2021-09-10 16:01:54 -0400

    testing finding gameWords algo

--- src/game.js
+++ src/game.js
@@ -31,13 +31,48 @@ async function game() {
 
 function findWords(cell, root) {
     const words = [];
-    //I need to loop through all of the neighbors to find words
-    console.log(cell[1])
+    const queue = [[root, cell]];
+    const visitedCells = [cell];
 
-    cell[1].neighbors.forEach(n => {
-        
-    })
-    if (cell[1].coordinates === '3,3') console.log(root)
+    //for ele, tree is position 0 and cell is position 1
+    //for cell tile is position 0 and node is position 1
+    while (queue.length) {
+        let ele = queue.shift();
+        for (let i = 0; i < ele[1][1].neighbors.length; i++) {
+            if (ele[0].complete) {
+                let currentWord = fetchWord(ele[0])
+                if (!words.includes(currentWord)) {
+                    words.push(currentWord)
+                }
+            }
+            if (!visitedCells.includes(ele[1][1].neighbors[i])) {
+                visitedCells.push(ele[1][1])
+                //grab the char of the current neighbor
+                console.log(`${ele[1][1].coordinates} ${ele[1][1].ch}`)
+                console.log(ele[1][1].neighbors[i][1].ch)
+                let char = ele[1][1].neighbors[i][1].ch
+                let subTree = ele[0]
+                if (subTree.map[char]) {
+                    subTree = subTree.map[char];
+                    //I just need to give it the next cell
+                    queue.push([subTree, ele[1][1].neighbors[i]])
+                }
+            }
+        }
+    }
+    // console.log(cell[1].coordinates)
+    // console.log(words)
+}
+
+function fetchWord(currentNode) {
+    let nodeCheck = currentNode;
+    let word = '';
+    while(nodeCheck.parent !== null) {
+        word = nodeCheck.ch + word;
+        nodeCheck = nodeCheck.parent;
+    }
+    
+    return word;
 }
 
 //set up gridNodes with their respective divs in an inner array length 2
@@ -66,7 +101,7 @@ function setUpGrid() {
             tile.className = "game-tile"
             tile.style.left = j * 100 + "px";
             tile.style.top = i * 100 + "px";
-            tile.innerHTML = letter
+            tile.innerHTML = `${letter} [${i},${j}]`
             gameBoardContainer.appendChild(tile)
 
             cell.push(tile)
