839cf3fd282a3fcf2372382f7ae84522463d6591
Jonathan <jonathan.diaz714@gmail.com>
2024-07-05 16:16:53 -0400

    additional styling to maximize gameboard

--- scr2/main.js
+++ scr2/main.js
@@ -1,3 +1,12 @@
 import definitions from '../src/definitions.json' with { type: "json" };
 
 console.log(definitions['TEST'])
+
+// just build the project in one js file and then focus on how you want to organize it
+
+class GameTile {
+  constructor() {
+    this.div = document.createElement('div');
+    this.children = [];
+  }
+}
