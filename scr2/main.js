ab03453c4db14cc992dbba55a599b3180d9f7f05
Jonathan <jonathan.diaz714@gmail.com>
2024-07-05 17:00:09 -0400

    fixed original game with the words array

--- scr2/main.js
+++ scr2/main.js
@@ -1,12 +1,36 @@
 import definitions from '../src/definitions.json' with { type: "json" };
-
-console.log(definitions['TEST'])
+const words = Object.keys(definitions);
 
 // just build the project in one js file and then focus on how you want to organize it
-
-class GameTile {
-  constructor() {
-    this.div = document.createElement('div');
+// I should calculate the best possible game board before populating the actual board
+// This way once I have a board with 70 words or more I can just populate the board
+class GridNode {
+  constructor(i, j) {
+    this.i = i;
+    this.j = j;
     this.children = [];
+    this.char = "AABCDEEFGHIIJKLMNOOPQRSSTUUVWXYZ"[Math.floor(Math.random() * 32)];
   }
 }
+
+// Now I want to build a 4 x 4 matrix with these gridNodes
+
+const matrix = [
+  [ new GridNode(0, 0), new GridNode(0, 1), new GridNode(0, 2), new GridNode(0, 3) ],
+  [ new GridNode(1, 0), new GridNode(1, 1), new GridNode(1, 2), new GridNode(1, 3) ],
+  [ new GridNode(2, 0), new GridNode(2, 1), new GridNode(2, 2), new GridNode(2, 3) ],
+  [ new GridNode(3, 0), new GridNode(3, 1), new GridNode(3, 2), new GridNode(3, 3) ],
+]
+
+// Now I need to traverse through this matrix to find all of the possible words that can be made with it
+// I need to first build a Trie with all of the words in the definitions object
+
+class TrieNode {
+  constructor(char) {
+    this.char = char;
+    this.children = {};
+    this.word = null;
+  }
+}
+
+const root = new TrieNode(null);
--- src/game.js
+++ src/game.js
@@ -1,21 +1,25 @@
 import { trieNode, add, fetchWord } from "./trie.js";
 import { gridNode, setUpGrid, findWords } from "./gridNode.js";
 import { modal, timer } from "./util.js";
-
-let globalDictionary = null;
-
-async function getDictionay() {
-  const response = await fetch("dictionary.txt");
-  const data = await response.text();
-  globalDictionary = data.split(/\r?\n/).filter((word) => {
+import definitions from '../src/definitions.json' with { type: "json" };
+const words = Object.keys(definitions).filter((word) => {
     return word.length > 2;
   });
-}
 
-async function game() {
-  await getDictionay();
+// let globalDictionary = null;
+
+// async function getDictionay() {
+//   const response = await fetch("dictionary.txt");
+//   const data = await response.text();
+//   words = data.split(/\r?\n/).filter((word) => {
+//     return word.length > 2;
+//   });
+// }
+
+function game() {
+  // await getDictionay();
   const root = new trieNode(null);
-  for (const item of globalDictionary) add(item, 0, root);
+  for (const item of words) add(item, 0, root);
 
   modal();
 
