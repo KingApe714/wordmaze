f6e742969df83917ef644ce56c330644155462fd
Jonathan <jonathan.diaz714@gmail.com>
2024-07-05 21:08:45 -0400

    finished the logic for generating baords

--- scr2/main.js
+++ scr2/main.js
@@ -1,30 +1,6 @@
 import definitions from '../src/definitions.json' with { type: "json" };
 const words = Object.keys(definitions);
 
-// just build the project in one js file and then focus on how you want to organize it
-// I should calculate the best possible game board before populating the actual board
-// This way once I have a board with 70 words or more I can just populate the board
-class GridNode {
-  constructor(i, j) {
-    this.i = i;
-    this.j = j;
-    this.children = [];
-    this.char = "AABCDEEFGHIIJKLMNOOPQRSSTUUVWXYZ"[Math.floor(Math.random() * 32)];
-  }
-}
-
-// Now I want to build a 4 x 4 matrix with these gridNodes
-
-const matrix = [
-  [ new GridNode(0, 0), new GridNode(0, 1), new GridNode(0, 2), new GridNode(0, 3) ],
-  [ new GridNode(1, 0), new GridNode(1, 1), new GridNode(1, 2), new GridNode(1, 3) ],
-  [ new GridNode(2, 0), new GridNode(2, 1), new GridNode(2, 2), new GridNode(2, 3) ],
-  [ new GridNode(3, 0), new GridNode(3, 1), new GridNode(3, 2), new GridNode(3, 3) ],
-]
-
-// Now I need to traverse through this matrix to find all of the possible words that can be made with it
-// I need to first build a Trie with all of the words in the definitions object
-
 class TrieNode {
   constructor(char) {
     this.char = char;
@@ -33,4 +9,111 @@ class TrieNode {
   }
 }
 
-const root = new TrieNode(null);
+const buildTrie = () => {
+  const root = new TrieNode(null);
+  
+  for (const word of words) {
+    let current = root;
+  
+    for (const char of word) {
+      if (!(char in current.children)) {
+        current.children[char] = new TrieNode(char);
+      }
+      current = current.children[char];
+    }
+
+    current.word = word;
+  }
+
+  return root;
+}
+
+const generateChar = () => "AABCDEEFGHIIJKLMNOOPQRSSTUUVWXYZ"[Math.floor(Math.random() * 32)];
+
+const generateMatrix = () => {
+  return [
+    [ generateChar(), generateChar(), generateChar(), generateChar() ],
+    [ generateChar(), generateChar(), generateChar(), generateChar() ],
+    [ generateChar(), generateChar(), generateChar(), generateChar() ],
+    [ generateChar(), generateChar(), generateChar(), generateChar() ],
+  ]
+}
+
+const isValid = (i, j, visited, board, trie) => {
+  return (
+    i >= 0 && i < 4 &&
+    j >= 0 && j < 4 &&
+    !visited.has(`${i},${j}`) &&
+    board[i][j] in trie.children
+  )
+}
+
+const coords = [
+  [ -1, -1 ],
+  [ -1, 0 ],
+  [ -1, 1 ],
+  [ 0, 1 ],
+  [ 1, 1 ],
+  [ 1, 0 ],
+  [ 1, -1 ],
+  [ 0, -1 ]
+]
+
+const bfs = (matrix, root, idx, jdx) => {
+  const queue = [ [ root, idx, jdx, new Set([ `${idx},${jdx}` ]) ] ];
+  const foundWords = []
+
+  while (queue.length) {
+    const [ trie, i, j, visited ] = queue.shift();
+    
+    if (trie.word) {
+      foundWords.push(trie.word);
+    }
+
+    for (const [ deltaI, deltaJ ] of coords) {
+      const [ nextI, nextJ ] = [ i + deltaI, j + deltaJ ];
+
+      if (isValid(nextI, nextJ, visited, matrix, trie)) {
+        const char = matrix[nextI][nextJ];
+        const nextVisited = new Set(visited);
+        const nextTrie = trie.children[char];
+
+        nextVisited.add(`${nextI},${nextJ}`);
+        queue.push([ nextTrie, nextI, nextJ, nextVisited ]);
+      }
+    }
+  }
+
+  return foundWords;
+}
+
+// I need this function to bfs the board to find all possible words
+// It'll keep being called until I have a board that has the number of words that I want
+const boardCheck = (board, root) => {
+  const words = [];
+
+  for (let i = 0; i < 4; i += 1) {
+    for (let j = 0; j < 4; j += 1) {
+      const char = board[i][j];
+      const node = root.children[char];
+      const currentFound = bfs(board, node, i, j);
+      words.push(...currentFound);
+    }
+  }
+
+  return words;
+}
+
+// I need to build the ancestory nodes from here to send to the main script to build a board out of
+// I need to keep calling the generateMatrix function until it gives me a board with 90 words or more
+const buildBoard = () => {
+  const root = buildTrie();
+  let current = generateMatrix();
+
+  while (boardCheck(current, root).length < 100) {
+    current = generateMatrix();
+  }
+
+}
+
+buildBoard();
