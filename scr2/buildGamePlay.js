30b5db9743094a19bb0f3e3636321506e8894740
Jonathan <jonathan.diaz714@gmail.com>
2024-07-13 01:58:14 -0400

    begun to capture swipte event coordinates

--- scr2/buildGamePlay.js
+++ scr2/buildGamePlay.js
@@ -54,10 +54,6 @@ export const gamePlay = (ancestoryMatrix) => {
   const lines = [];
   let current = null;
 
-  const boardData = gameBoard.getBoundingClientRect();
-  const height = boardData.height;
-  const width = boardData.width;
-
   gameBoard.addEventListener("touchstart", (e) => {
     e.preventDefault();
 
@@ -68,10 +64,41 @@ export const gamePlay = (ancestoryMatrix) => {
       }
     }
 
-    console.log(e.touches[0]);
+    const [idx, jdx] = calculateCoords(e.touches[0]);
 
-    const i = e.touches[0].clientY - top;
-    const j = e.touches[0].clientX - left;
+    current = ancestoryMatrix[idx][jdx];
+
+    console.log(current);
+  });
+
+  gameBoard.addEventListener("touchmove", (e) => {
+    e.preventDefault();
+
+    const [idx, jdx] = calculateCoords(e.touches[0]);
+
+    // this method is now successfully capturing the coordinates as I need them while the user is swiping.
+    // Now I need to ensure that we only calculate user building a word if and only if we are entering new coordinates.
+    // consider an if statement that takes current.i and idx and current.j and jdx and compares them.
+
+    if (current.i !== idx || current.j !== jdx) {
+      console.log([idx, jdx]);
+      current = ancestoryMatrix[idx][jdx];
+    }
+  });
+
+  // I need to remove the discrepency from the corners. Each tile isn't absolutely 25% of the board, they are actually smaller than 25%. they are 20% of the board. this means that 2.5% above the tile and 2.5% below the tile does not have to be listened for. If I don't do this then I won't be able to able to make diagonal moves
+  // this function can handle this for me. I need to figure out how to make it do that for me and then return -1 on either the idx or the jdx or both if they are not within the bound of the tile.
+  // I may have to make it listen for a smaller distance as the swipe area is quite big
+  const calculateCoords = (touchEvent) => {
+    const boardData = gameBoard.getBoundingClientRect();
+    const height = boardData.height;
+    const width = boardData.width;
+
+    const top = boardData.top;
+    const left = boardData.left;
+
+    const i = touchEvent.clientY - top;
+    const j = touchEvent.clientX - left;
 
     const numI = (i / height) * 100;
     const numJ = (j / width) * 100;
@@ -79,12 +106,8 @@ export const gamePlay = (ancestoryMatrix) => {
     const idx = Math.floor((numI - 1) / 25);
     const jdx = Math.floor((numJ - 1) / 25);
 
-    console.log(`idx = ${idx}, jdx = ${jdx}`);
-  });
-
-  gameBoard.addEventListener("touchmove", (e) => {
-    e.preventDefault();
-  });
+    return [idx, jdx];
+  };
 
   const highlightPath = (node) => {
     if (node.word) {
