e7f35c3595c3eb7859c44855bf005cdcc1bc3509
Jonathan <jonathan.diaz714@gmail.com>
2024-07-13 02:41:53 -0400

    seting up the touch move logic

--- scr2/buildGamePlay.js
+++ scr2/buildGamePlay.js
@@ -67,8 +67,49 @@ export const gamePlay = (ancestoryMatrix) => {
     const [idx, jdx] = calculateCoords(e.touches[0]);
 
     current = ancestoryMatrix[idx][jdx];
+    current.innerGameDiv.style.backgroundColor = "rgba(255, 0, 0, 0.4)";
+    current.visited = true;
+    current.lastVisited = true;
+    highlightPath(current);
+  });
 
-    console.log(current);
+  // this is the exact same code as mouse up. consider consolidating
+  gameBoard.addEventListener("touchend", (e) => {
+    e.preventDefault();
+
+    svg.innerHTML = "";
+
+    for (let i = 0; i < 4; i += 1) {
+      for (let j = 0; j < 4; j += 1) {
+        const node = ancestoryMatrix[i][j];
+
+        // here I am looking at a tile that cereates a word
+        if (node.lastVisited && node.current && node.current.word) {
+          const rootNode = node.current.rootNode || node;
+          const current = node.current;
+
+          if (!current.found) rootNode.foundWordCount += 1;
+          current.found = true;
+          current.clueDiv.style.backgroundColor = "green";
+
+          if (rootNode.foundWordCount === rootNode.wordCount) {
+            rootNode.complete = true;
+            rootNode.gameDiv.style.filter = "hue-rotate(90deg) saturate(200%)";
+
+            for (const div of rootNode.clueCharContainers) {
+              div.style.backgroundColor = "green";
+            }
+          }
+        }
+
+        node.active = false;
+        node.visited = false;
+        node.lastVisited = false;
+        node.current = node;
+        node.lines.length = 0;
+        node.innerGameDiv.style.backgroundColor = "";
+      }
+    }
   });
 
   gameBoard.addEventListener("touchmove", (e) => {
@@ -77,9 +118,7 @@ export const gamePlay = (ancestoryMatrix) => {
     const [idx, jdx] = calculateCoords(e.touches[0]);
 
     if (idx !== -1 && jdx !== -1 && (current.i !== idx || current.j !== jdx)) {
-      console.log([idx, jdx]);
-      current = ancestoryMatrix[idx][jdx];
-      console.log(current);
+      const node = ancestoryMatrix[idx][jdx];
     }
   });
 
