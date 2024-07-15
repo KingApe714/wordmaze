4f9afe0cc3f655dcbd9bbd8f1819d9eef4e2040c
Jonathan <jonathan.diaz714@gmail.com>
2024-07-14 20:04:24 -0400

    set new game as root

--- scr2/endRound.js
+++ scr2/endRound.js
@@ -2,4 +2,14 @@ export const endRound = (user, ancestoryMatrix) => {
   console.log(user.points);
   console.log(user);
   console.log(ancestoryMatrix);
+  const modalBtn = document.querySelector(".modal-button");
+  const modalBg = document.querySelector(".modal-bg");
+  const modalClose = document.querySelector(".modal-close");
+  const modalChild = document.querySelector(".modal-child");
+  const modalTitle = document.querySelector(".modal-title");
+
+  const modalInner = document.querySelector(".modal-inner");
+  modalBg.classList.add("bg-active");
+
+  // from here i want to display to the user all of the board data and their points and demerites
 };
--- scr2/main.js
+++ scr2/main.js
@@ -7,9 +7,16 @@ import { startTimer } from "./timer.js";
 import { endRound } from './endRound.js';
 
 // I believe that I need to create a user object that will gather the points and the demerites the user has gained throughout the game
+// the demerits should have the path as the key and the amount of points it costed them
+// I should also create a demerits.json to easily compute this
+// I need a way to capture that paths that the user creates to stringify it and use it as a key then have the value be the demerit
+// at touchend_mouseup I can check to see if we have found a word, if not, then use the line to score the demerit, and if it is a word the user found already we can 
 const user = {
   points: 0,
-  demerits: 0
+  demerits: {
+    nonwords: {},
+    foundWords: {}
+  }
 }
 const root = buildTrie(definitions);
 const gameBoard = buildBoard(root);
